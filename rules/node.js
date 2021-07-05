14b9715eb9781a85462e155d87417435a1a2b28f
yangin <422918408@qq.com>
2021-07-05 09:22:55 +0800

    FEAT: 添加了node、promise插件

--- rules/node.js
+++ rules/node.js
@@ -2,17 +2,24 @@ module.exports = {
     env: {
       node: true
     },
-  
+    plugins: [
+      'node'
+    ],
     rules: {
         // 要求回调函数中有容错处理
         'handle-callback-err': [ 'error', '^(err|error)$' ],
+        
         // 禁止调用 require 时使用 new 操作符
         'no-new-require': [ 'error' ],
+
         // 禁止对 __dirname 和 __filename 进行字符串连接
         'no-path-concat': [ 'error' ],
         
-        // 'node/no-deprecated-api': [ 'error' ],
-        // 'node/process-exit-as-throw': [ 'error' ],
+        // 禁止使用已弃用的api
+        'node/no-deprecated-api': [ 'error' ],
+
+        //要求process.exit() 与 throw 在相同代码路径下
+        'node/process-exit-as-throw': [ 'error' ],
     }
   };
   
---
+++ rules/promise.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        'promise'
+      ],
+    rules: {
+        // 要求在声明参数名时，必须采用标准的 resolve, reject，禁止采用其他的自定义名称
+        'promise/param-names': [ 'error' ],
+    }
+  };
+  
