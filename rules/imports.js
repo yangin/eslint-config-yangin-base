978944983179f266b9fcbc0564dde86a2c7fc822
yangjin <yangjin@people-energy.com.cn>
2022-05-11 11:52:03 +0800

    FEAT: 新增import规则

--- rules/imports.js
+++ rules/imports.js
@@ -12,7 +12,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        extensions: [ '.mjs', '.js', '.json', '.ts', '.tsx' ]
+        extensions: [ '.mjs', '.js', '.jsx', '.json', '.ts', '.tsx' ]
       }
     },
     'import/extensions': [
@@ -31,28 +31,40 @@ module.exports = {
   },
 
   rules: {
-    // 【强制】报告任何无效的import,如相同名字
+    // 报告任何无效的import,如相同名字
     'import/export': [ 'error' ],
 
-    // 【强制】确保所有的import在声明之前
+    // 所有.js/.ts/.jsx/.tsx文件省略后缀，其他的带后缀，除了包导入。
+    'import/extensions': [ 'error', 'ignorePackages', { 'js': 'never', 'jsx': 'never', 'ts': 'never', 'tsx': 'never' } ],
+
+    // 确保所有的import在声明之前
     'import/first': [ 'error' ],
 
-    // 强制在最后一个顶级导入语句或要求调用之后有一个空行
-    'newline-after-import': [ 'error', { 'count': 1 } ],
+    // 在最后一个顶级导入语句或要求调用之后有一个空行
+    'import/newline-after-import': [ 'error', { 'count': 1 } ],
 
-    // 【强制】报告在多个地方重复导入同一模块 --fix
+    // 禁止使用绝对路径
+    'import/no-absolute-path': [ 'error' ],
+
+    // 报告在多个地方重复导入同一模块 --fix
     'import/no-duplicates': [ 'error' ],
 
-    // 【强制】禁止通过 var 或 let 使用可变导出
+    // 禁止通过 var 或 let 使用可变导出
     'import/no-mutable-exports': [ 'error' ],
 
-    // 【强制】禁止使用默认导出作为本地命名导入，如 export default 'foo'; export const bar = 'baz';
+    // 禁止使用默认导出作为本地命名导入，如 export default 'foo'; export const bar = 'baz';
     'import/no-named-default': [ 'error' ],
 
-    // 【强制】禁止导入中的 webpack 加载器语法
+    // 禁止自身引用
+    'import/no-self-import': [ 'error' ],
+
+    // 禁止无用的路径片段，及index.js路径片段
+    "import/no-useless-path-segments": ["error", { noUselessIndex: true }],
+
+    // 禁止导入中的 webpack 加载器语法
     'import/no-webpack-loader-syntax': [ 'error' ],
 
-    // 【强制】定义import的排序 --fix
+    // 定义import的排序 --fix
     'import/order': [ 'error', { 'groups': [ 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type' ] } ]
   }
 }
